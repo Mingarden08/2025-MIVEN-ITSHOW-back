5aaad0bf695172998a973e419137af2385704db2
Mingarden08 <jeongone.min@gmail.com>
2025-06-04 18:57:42 +0900

    [ADD] Application edit

--- api/src/main/java/com/bookmoment/api/ApiApplication.java
+++ api/src/main/java/com/bookmoment/api/ApiApplication.java
@@ -3,7 +3,11 @@ package com.bookmoment.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
+@EnableJpaAuditing
 @Slf4j
 @SpringBootApplication
 public class ApiApplication {
